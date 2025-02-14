91899acc14cd9e65f560af5033969612bd7ceaaf
locanasjaya704 <locanasjayaeunice_bsit@plmun.edu.ph>
2025-02-14 10:02:59 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -1,4 +1,6 @@
-CREATE TABLE `Students` ( `StudentID` INT PRIMARY KEY AUTO_INCREMENT, 
-`FirstName` VARCHAR(50) NOT NULL, 
-`LastName` VARCHAR(50) NOT NULL, 
-`EnrollmentDate` DATE );
+CREATE TABLE `Students` (
+   `StudentID` INT PRIMARY KEY AUTO_INCREMENT,     
+   `FirstName` VARCHAR(50) NOT NULL,
+   `LastName` VARCHAR(50) NOT NULL,
+   `EnrollmentDate` DATE
+);
